e6fa806371fd5e17b05e571802d07688fe14c7ab
v-lacayo <vjlatino@gmail.com>
2018-08-11 00:28:28 -0600

    First Structure

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './img/movie-icon.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container primary">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="index.html">
+                <img src={logo} className="App-logo" alt="logo" title="movieFinder" />
+              </a>
+            </div>
+          </div>
+        </nav>
+
+        <div className="float">
+          <div className="container">
+            <h3>Search the movies you want it!</h3>
+            <form id="search">
+              <input className="form-control" id="searchTxt" placeholder="Amor con Frijoles"/>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
